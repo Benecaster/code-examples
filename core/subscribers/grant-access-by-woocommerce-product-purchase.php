cc10045c8d0c3ae533473d04a7826f599330ff94
Anadar Professional Services LLC <152109841+AnadarProSvcs@users.noreply.github.com>
2026-08-01 08:38:07 -0700

    Update recipe: Grant access based on WooCommerce product purchase

--- core/subscribers/grant-access-by-woocommerce-product-purchase.php
+++ core/subscribers/grant-access-by-woocommerce-product-purchase.php
@@ -1,4 +1,6 @@
+<?php
 // Grant access based on WooCommerce product purchase
+
 // Grant free-preview tagged episodes to all logged-in users.
 add_filter( 'benecaster_episode_is_accessible', function( bool $access, int $episode_id, int $user_id, string $tier_slug ): bool {
     if ( $user_id > 0 && has_term( 'free-preview', 'category', $episode_id ) ) {
