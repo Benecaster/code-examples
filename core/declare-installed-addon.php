0dff1d4c5d7c26b185fae0929083bb1d86f7fa6d
Anadar Professional Services LLC <152109841+AnadarProSvcs@users.noreply.github.com>
2026-08-01 08:42:34 -0700

    Update recipe: Tell the Add-ons screen that this add-on plugin is loaded

--- core/declare-installed-addon.php
+++ core/declare-installed-addon.php
@@ -1,4 +1,6 @@
+<?php
 // Tell the Add-ons screen that this add-on plugin is loaded
+
 add_action( 'benecaster_boot', function (): void {
     add_filter( 'benecaster_installed_addons', static function ( array $slugs ): array {
         $slugs[] = 'benecaster-addon-guest-manager';
