18cf41b2303ecdfc4c293d34dbca9c855573a445
Anadar Professional Services LLC <152109841+AnadarProSvcs@users.noreply.github.com>
2026-09-03 16:31:16 -0400

    Update recipe: Inject a synthetic reference group from an add-on

--- core/episodes/inject-synthetic-reference-group.php
+++ core/episodes/inject-synthetic-reference-group.php
@@ -5,10 +5,12 @@ add_filter( 'benecaster_reference_groups', function ( array $groups, int $show_i
     if ( ! my_addon_is_active_for_show( $show_id ) ) {
         return $groups;
     }
+
     array_unshift( $groups, [
         'id'            => 0,
         'name'          => __( 'Tracked Links', 'my-addon' ),
         'display_order' => -1,
     ] );
+
     return $groups;
 }, 10, 2 );
